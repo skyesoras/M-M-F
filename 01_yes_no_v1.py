396224fc40cf936d1a7ad7af370495ca4b94663d
skyesoras <134565254+skyesoras@users.noreply.github.com>
2023-06-06 09:32:18 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,16 @@
+# functions go here
+
+
+# main routine goes here
+print("hello world")
+
+want_instructions = input("do you want to read the instructions? ")
+print(want_instructions)
+
+if want_instructions == "yes":
+    print("instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes / no")
+    
